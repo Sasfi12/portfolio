430f28c48908957f69675772819f4c3110abd64b
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-05-26 16:19:12 +0200

    managed to make a simple frontend display

--- app/Http/Controllers/FolioController.php
+++ app/Http/Controllers/FolioController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folio;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class FolioController extends Controller
 {
-    //
+    public function index() {
+        $folios = Folio::where('id' , '=' , 1)->get();
+        return Inertia::render("welcome" , ["folios" => $folios[0]]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\FolioController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/', [FolioController::class , "index"]);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {

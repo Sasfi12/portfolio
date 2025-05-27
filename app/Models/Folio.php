f9bcad75cc2f2331af2608ef6f53616cd4a02325
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-05-27 15:30:44 +0200

    Folio linking done

--- app/Models/Folio.php
+++ app/Models/Folio.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folio extends Model
 {
-     public function project() {
-        
+     public function projects() {
+        $this->hasMany(Project::class);
+     }
+     public function experiences() {
+        $this->hasMany(Experience::class);
+     }
+     public function skills() {
+        $this->hasMany(Skill::class);
      }
 }

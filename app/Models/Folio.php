508d150e74e61165c98de45cde88a29faff6a982
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-05-28 11:25:13 +0200

    small update

--- app/Models/Folio.php
+++ app/Models/Folio.php
@@ -8,7 +8,7 @@ class Folio extends Model
 {
      public function projects() {
         return $this->hasMany(Project::class , "user_id" , "id");
-     }
+     }        
      public function experiences() {
         return $this->hasMany(Experience::class , "user_id" , "id");
      }
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,9 +1,10 @@
+import "./main.css"
 function Welcome({folios}) {
     console.log(folios)
     return (
         <div>
-            <h1>Welcome to {folios.prénom} {folios.nom}'s portfolio !</h1>
-            <img src={folios.photo} alt={folios.prénom + " " + folios.nom + " picture" } />
+            <h1 className="text-9xl">Welcome to {folios.prénom} {folios.nom}'s portfolio !</h1>
+            <img className="h-20 w-20" src={folios.photo} alt={folios.prénom + " " + folios.nom + " picture" } />
         </div>
     )
 }
